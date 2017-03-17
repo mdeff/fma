f531721c2e3e500d81b2be84898742370b72fff1
Michaël Defferrard <michael.defferrard@epfl.ch>
2017-03-17 19:04:01 +0100

    freemusicarchive pypi package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='freemusicarchive',
+      version='0.0.0',
+      description='Free Music Archive',
+      url='https://github.com/mdeff/fma',
+      author='Michaël Defferrard',
+      author_email='michael.defferrard@epfl.ch',
+      license='MIT')
