mdeff/fma

commit a000220298a1369df52bc315a4e2808abf9b3aa7

genres: parent_id --> parent

utils.py
@@ -109,7 +109,7 @@ class FreeMusicArchive:
         return genre_ids, genre_titles
 
     def get_all_genres(self):
-        df = pd.DataFrame(columns=['genre_parent_id', 'genre_title',
+        df = pd.DataFrame(columns=['genre_parent', 'genre_title',
                                    'genre_handle', 'genre_color'])
         df.index.rename('genre_id', inplace=True)
 
@@ -150,7 +150,7 @@ class Genres:
         def create_tree(root_id, node_p, depth):
             if depth == 0:
                 return
-            children = self.df[self.df['parent_id'] == root_id]
+            children = self.df[self.df['parent'] == root_id]
             for child in children.iterrows():
                 genre_id = child[0]
                 node_c = create_node(genre_id)
@@ -167,15 +167,14 @@ class Genres:
 
     def find_roots(self):
         roots = []
-        for genre in self.df.iterrows():
-            genre_id = genre[0]
-            parent_id = genre[1]['parent_id']
-            title = genre[1]['title']
-            if parent_id == 0:
-                roots.append(genre_id)
-            elif parent_id not in self.df.index:
+        for gid, row in self.df.iterrows():
+            parent = row['parent']
+            title = row['title']
+            if parent == 0:
+                roots.append(gid)
+            elif parent not in self.df.index:
                 msg = '{} ({}) has parent {} which is missing'.format(
-                        genre_id, title, parent_id)
+                        gid, title, parent)
                 raise RuntimeError(msg)
         return roots
 

analysis.ipynb
@@ -462,7 +462,7 @@
    },
    "outputs": [],
    "source": [
-    "counts = pd.Series(index=genres.loc[genres['parent_id'] == 0, 'title'].values, name='#artists')\n",
+    "counts = pd.Series(index=genres.loc[genres['parent'] == 0, 'title'].values, name='#artists')\n",
     "for genre in counts.index:\n",
     "    counts[genre] = len(tracks.loc[tracks['track', 'genre_top'] == genre, ('artist', 'id')].unique())\n",
     "counts.sort_values(ascending=False).plot.bar(figsize=(17, 5))\n",
@@ -494,7 +494,7 @@
     "assert a == b\n",
     "\n",
     "print('{} top-level genres'.format(len(a)))\n",
-    "genres[genres['parent_id'] == 0].sort_values('#tracks', ascending=False)"
+    "genres[genres['parent'] == 0].sort_values('#tracks', ascending=False)"
    ]
   },
   {

creation.ipynb
@@ -717,7 +717,7 @@
    "outputs": [],
    "source": [
     "genres.drop(['genre_handle', 'genre_color'], axis=1, inplace=True)\n",
-    "genres.rename(columns={'genre_parent_id': 'parent_id', 'genre_title': 'title'}, inplace=True)"
+    "genres.rename(columns={'genre_parent_id': 'parent', 'genre_title': 'title'}, inplace=True)"
    ]
   },
   {
@@ -730,8 +730,8 @@
    },
    "outputs": [],
    "source": [
-    "genres['parent_id'].fillna(0, inplace=True)\n",
-    "genres = genres.astype({'parent_id': int})"
+    "genres['parent'].fillna(0, inplace=True)\n",
+    "genres = genres.astype({'parent': int})"
    ]
   },
   {
@@ -746,19 +746,19 @@
    "source": [
     "# 13 (Easy Listening) has parent 126 which is missing\n",
     "# --> a root genre on the website, although not in the genre menu\n",
-    "genres.at[13, 'parent_id'] = 0\n",
+    "genres.at[13, 'parent'] = 0\n",
     "\n",
     "# 580 (Abstract Hip-Hop) has parent 1172 which is missing\n",
     "# --> listed as child of Hip-Hop on the website\n",
-    "genres.at[580, 'parent_id'] = 21\n",
+    "genres.at[580, 'parent'] = 21\n",
     "\n",
     "# 810 (Nu-Jazz) has parent 51 which is missing\n",
     "# --> listed as child of Easy Listening on website\n",
-    "genres.at[810, 'parent_id'] = 13\n",
+    "genres.at[810, 'parent'] = 13\n",
     "\n",
     "# 763 (Holiday) has parent 763 which is itself\n",
     "# --> listed as child of Sound Effects on website\n",
-    "genres.at[763, 'parent_id'] = 16\n",
+    "genres.at[763, 'parent'] = 16\n",
     "\n",
     "# Todo: should novely be under Experimental? It is alone on website."
    ]
@@ -791,7 +791,7 @@
    "outputs": [],
    "source": [
     "def get_parent(genre, track_all_genres=None):\n",
-    "    parent = genres.at[genre, 'parent_id']\n",
+    "    parent = genres.at[genre, 'parent']\n",
     "    if track_all_genres is not None:\n",
     "        track_all_genres.append(genre)\n",
     "    return genre if parent == 0 else get_parent(parent, track_all_genres)\n",
@@ -1053,7 +1053,7 @@
    },
    "outputs": [],
    "source": [
-    "tmp = genres[genres['parent_id'] == 0].reset_index().set_index('title')\n",
+    "tmp = genres[genres['parent'] == 0].reset_index().set_index('title')\n",
     "tmp['#tracks_medium'] = fma_medium['track', 'genre_top'].value_counts()\n",
     "tmp.sort_values('#tracks_medium', ascending=False)"
    ]

commit 4c6b31ff94625adfd4e0ee4052674126144175a6

correct bug introduced in a000220298a1369df52bc315a4e2808abf9b3aa7

utils.py
@@ -109,7 +109,7 @@ class FreeMusicArchive:
         return genre_ids, genre_titles
 
     def get_all_genres(self):
-        df = pd.DataFrame(columns=['genre_parent', 'genre_title',
+        df = pd.DataFrame(columns=['genre_parent_id', 'genre_title',
                                    'genre_handle', 'genre_color'])
         df.index.rename('genre_id', inplace=True)
 